5949338762b3a249634a464d57245875f6345675
Chris Stock <christopher.h.stock@gmail.com>
2019-11-19 01:27:02 -0800

    updated scraper

--- autolocal/legistar_scraper.py
+++ autolocal/legistar_scraper.py
@@ -14,85 +14,8 @@ from selenium.common.exceptions import StaleElementReferenceException
 from tqdm import tqdm
 import time
 
+import os
 
-data_dir = '../data/scraping/scraped_tables/'
-
-CITY_ARGS = [
-    {
-    'city_name': 'hayward',
-    'scrape_url': 'https://hayward.legistar.com/Calendar.aspx',
-    },
-    {
-    'city_name': 'santa_clara',
-    'scrape_url': 'https://santaclara.legistar.com/Calendar.aspx',
-    }, 
-    {
-    'city_name': 'mountain_view',
-    'scrape_url': 'https://mountainview.legistar.com/Calendar.aspx',
-    },
-    {
-    'city_name': 'sunnyvale',
-    'scrape_url': 'https://sunnyvaleca.legistar.com/Calendar.aspx',
-    },
-    {
-    'city_name': 'san_jose',
-    'scrape_url': 'https://sanjose.legistar.com/Calendar.aspx'
-    },
-    {
-    'city_name': 'cupertino',
-    'scrape_url': 'https://cupertino.legistar.com/Calendar.aspx'
-    },
-    {
-    'city_name': 'san_mateo_county',
-    'scrape_url': 'https://sanmateocounty.legistar.com/Calendar.aspx'
-    },    
-    {
-    'city_name': 'burlingame',
-    'scrape_url': 'https://burlingameca.legistar.com/Calendar.aspx'
-    },
-    {
-    'city_name': 'san_leandro',
-    'scrape_url': 'https://sanleandro.legistar.com/Calendar.aspx'
-    },    
-    {
-    'city_name': 'alameda',
-    'scrape_url': 'https://alameda.legistar.com/Calendar.aspx'
-    },   
-    {
-    'city_name': 'oakland',
-    'scrape_url': 'https://oakland.legistar.com/Calendar.aspx'
-    },
-    {
-    'city_name': 'san_francisco',
-    'scrape_url': 'https://sfgov.legistar.com/Calendar.aspx'
-    },
-    {
-    'city_name': 'hercules',
-    'scrape_url': 'https://hercules.legistar.com/Calendar.aspx'
-    },
-    {
-    'city_name': 'stockton',
-    'scrape_url': 'https://stockton.legistar.com/Calendar.aspx'
-    },
-    {
-    'city_name': 'south_san_francisco',
-    'scrape_url': 'https://ci-ssf-ca.legistar.com/Calendar.aspx'
-    },            
-    {
-    'city_name': 'mtc',
-    'scrape_url': 'https://mtc.legistar.com/Calendar.aspx'
-    }            
-]
-
-
-class TextChange(object):
-    def __init__(self, locator, text):
-        self.locator = locator
-        self.text = text
-
-    def __call__(self, driver):
-        actual_text = _find_element(driver, self.locator).text
-        return actual_text != self.text
 
 class LegistarScraper(object):
 
@@ -101,7 +24,8 @@ class LegistarScraper(object):
         city_name,
         scrape_url,
         base_url=None,
-        save_path=None,
+        save_dir='../data/scraping/scraped_tables',
+        headless=True,
         ):
                 
         self.city_name = city_name
@@ -110,9 +34,19 @@ class LegistarScraper(object):
             self.base_url = '{uri.scheme}://{uri.netloc}/'.format(uri=urlparse(self.scrape_url))
         else:
             self.base_url = base_url
+        self.save_dir = save_dir
+        if not os.path.exists(self.save_dir):
+            os.makedirs(self.save_dir)
 
-        self.driver = Firefox()
+        options = Options()    
+        options.headless = headless
+        self.driver = Firefox(options=options)
         self.driver.get(self.scrape_url)
+
+    def _click(self, item):
+        self.driver.execute_script("arguments[0].scrollIntoView();", item)
+        item.click()
+        return
 
 
     def _get_page_links(self, driver):
@@ -126,9 +60,57 @@ class LegistarScraper(object):
         elm_id = 'ctl00_ContentPlaceHolder1_gridCalendar_ctl00__0'
         return self.driver.find_element_by_id(elm_id).text.strip() #get_attribute('outerHTML')
 
-    def scrape_all_pages(self):
 
-        input('Manually select all years and all committees, then press enter. ')
+    def _wait_for_table_load(self, page_signature):
+        sig_match = True
+        while sig_match:
+            try:
+                time.sleep(0.1)
+                new_sig = self._get_page_signature()
+                sig_match = new_sig in [page_signature, '']         
+            except StaleElementReferenceException:
+                sig_match = False    
+        return
+
+    def scrape_all_pages(self, **filter_args):
+
+        dropdown_ids = [
+            ('years', 'ctl00_ContentPlaceHolder1_lstYears_Input', 'ctl00_ContentPlaceHolder1_lstYears_DropDown'),
+            ('bodies', 'ctl00_ContentPlaceHolder1_lstBodies_Input', 'ctl00_ContentPlaceHolder1_lstBodies_DropDown')
+        ]
+
+        page_signature = self._get_page_signature()
+
+        for field, input_id, dropdown_id in dropdown_ids:
+            dropdown_xpath = "//div[@id='{}']/div/ul/li".format(dropdown_id)
+            
+            # click on the dropdown menu
+            self._click(self.driver.find_element_by_id(input_id))
+
+            # wait for first list item to populate
+            parent_xpath = "//div[@id='{}']..".format(dropdown_id)
+            waiting = True
+            while waiting:
+                time.sleep(0.1)
+                dropdown_text = self.driver.find_element_by_xpath(dropdown_xpath).text
+                waiting = dropdown_text==''
+
+            # select filter term            
+            if field in filter_args.keys():
+                # if a particular filter is specified, use that
+                elms = self.driver.find_elements_by_xpath(dropdown_xpath)
+                filter_options = [elm.text for elm in elms]
+                i = filter_options.index(filter_args[field])
+                filter_elm = elms[i]
+            else:
+                # if not, select first option in dropdown
+                filter_elm = self.driver.find_element_by_xpath(dropdown_xpath)         
+            self._click(filter_elm)
+
+        # wait for table to load
+        self._wait_for_table_load(page_signature)
+
+        # input('Manually select all years and all committees, then press enter. ')
         # click through pages and save html
         c = 1
         page_data = []
@@ -146,16 +128,9 @@ class LegistarScraper(object):
                 # if it's not there and the list starts with '...', we are done.
                 else:
                     break
-            link.click()
+            self._click(link)
             if c > 1:
-                sig_match = True
-                while sig_match:
-                    try:
-                        time.sleep(0.1)
-                        new_sig = self._get_page_signature()
-                        sig_match = new_sig in [page_signature, '']         
-                    except StaleElementReferenceException:
-                        sig_match = False
+                self._wait_for_table_load(page_signature)
             print('Scraping page {}'.format(c))
             page_data.append(self.driver.page_source)
             c += 1
@@ -196,42 +171,86 @@ class LegistarScraper(object):
         num_cols = table.td.get('colspan')
         text_df = pd.DataFrame(text_data, columns=header_data)
         url_df = pd.DataFrame(url_data, columns=header_data)
-        df = pd.merge(text_df, url_df, left_index=True, right_index=True, suffixes=(' Text', ' URL'))
+        df = pd.merge(
+            text_df,
+            url_df,
+            left_index=True,
+            right_index=True,
+            suffixes=(' Text', ' URL'))
         
         return df
 
-    def extract_all_table_data(self, save_path=None, **kwargs):
-        if save_path is None:
-            save_path = '../data/scraping/scraped_tables/{}.csv'.format(self.city_name)
 
-        page_htmls = self.scrape_all_pages()
+    def extract_all_table_data(self, saving=True, **filter_args):
+        
+        # get htmls of pages
+        page_htmls = self.scrape_all_pages(**filter_args)
         print('Scraped {} pages'.format(len(page_htmls)))
+        
+        # convert to dataframes and concatenate
         page_dfs = [self.extract_table_data(page) for page in tqdm(page_htmls)]
         data = pd.concat(page_dfs)
         print('Recovered {} records'.format(len(data)))
-        if save_path:
-            print('Saving to {}'.format(save_path))
+        
+        # save
+        if saving:
+            save_path = os.path.join(self.save_dir, '{}.csv'.format(self.city_name))                        
             data.to_csv(save_path)
+            print('Saved to {}'.format(save_path))
+
         return data
 
 
-if __name__=='__main__':
-    import argparse
-    parser = argparse.ArgumentParser()
-    parser.add_argument("city_name")
-    args = parser.parse_args()
-
-    # get city to scrape (must be one of the cities in CITY_ARGS)
-    city_name = args.city_name
-    for city_args in CITY_ARGS:
-        if city_args['city_name']==city_name:
-            break
-
+def scrape_city(city_args, filter_args):
     # launch browser-based scraper
     scraper = LegistarScraper(**city_args)
-
+    
     # run scraping tool
-    page_data = scraper.extract_all_table_data()
-
+    page_data = scraper.extract_all_table_data(**filter_args)
+    
     # quit browser
     scraper.driver.quit()
+    
+    return None
+
+
+if __name__=='__main__':    
+    import argparse
+    import pandas as pd
+    parser = argparse.ArgumentParser()
+    parser.add_argument("city_list")
+    parser.add_argument("--out")
+    parser.add_argument("--year")
+    parser.add_argument("--bodies")
+    args = parser.parse_args()
+
+    # add search filters
+    filters = {}
+    if args.year:
+        filters['years'] = args.year
+    if args.bodies:
+        filters['bodies'] = args.bodies
+
+    # parse list of cities
+    city_csv_columns = ['city_name', 'scrape_url']    
+    city_df = pd.read_csv(args.city_list, header=None, names=city_csv_columns)
+
+    # scrape each city in turn    
+    for _, city_args in city_df.iterrows():        
+        city_args = dict(city_args)
+        if args.out:
+            city_args['save_dir'] = args.out        
+
+        scrape_city(city_args, filters)            
+
+    # import argparse
+    # parser = argparse.ArgumentParser()
+    # parser.add_argument("city_name")
+    # args = parser.parse_args()
+
+    # # get city to scrape (must be one of the cities in CITY_ARGS)
+    # city_name = args.city_name
+    # for city_args in CITY_ARGS:
+    #     if city_args['city_name']==city_name:
+    #         break
+
