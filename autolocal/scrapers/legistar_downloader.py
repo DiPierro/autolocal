9247fa4d42283ce0c2d95ce6866e1ad7fffac886
Chris Stock <christopher.h.stock@gmail.com>
2019-11-28 01:23:21 -0500

    updated legistar_downloader.py

--- autolocal/scrapers/legistar_downloader.py
+++ autolocal/scrapers/legistar_downloader.py
@@ -1,21 +1,20 @@
 import os
-import urllib
+import argparse
+from autolocal import S3DocumentManager
 
-from copy import deepcopy
-
-import pandas as pd
-import numpy as np
-
-from autolocal import DocumentManager
-
-
-documents = DocumentManager()
+documents = S3DocumentManager()
 
 def add_documents(doc_list_dir):
     doc_list_paths = [os.path.join(doc_list_dir, f) for f in os.listdir(doc_list_dir)]
+    print('Found paths:')
+    print('\n'.join(doc_list_paths))
     for p in doc_list_paths:
         documents.add_docs_from_csv(p)
 
 if __name__=='__main__':
-
-    add_documents(doc_list_dir)
+    
+    parser = argparse.ArgumentParser()
+    parser.add_argument('doc_list_dir')
+    args = parser.parse_args()
+    
+    add_documents(args.doc_list_dir)
