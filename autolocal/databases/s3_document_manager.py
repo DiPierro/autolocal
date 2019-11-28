31eae67090c8e6e1e08b7a3f1525ed1829a17db8
Chris Stock <christopher.h.stock@gmail.com>
2019-11-28 02:57:47 -0500

    updates

--- autolocal/databases/s3_document_manager.py
+++ autolocal/databases/s3_document_manager.py
@@ -221,7 +221,7 @@ class S3DocumentManager(DocumentManager):
         # csv should also contain field `url` in order to download anything        
         
         docs = pd.read_csv(csv_path, index_col=0)
-        with self.table.batch_writer(overwrite_by_pkeys=['partition_key', 'sort_key']) as batch:
+        with table.batch_writer(overwrite_by_pkeys=['doc_id']) as batch:
             for _, row in tqdm(docs.iterrows()):
                 self.add_doc(row, batch=batch)
             pass
