00af8f54f2c9e69b45eea4cfee45de500f73f5f3
Chris Stock <christopher.h.stock@gmail.com>
2019-12-02 16:10:19 -0500

    updated legistar scraper

--- autolocal/scrapers/legistar_logging.py
+++ autolocal/scrapers/legistar_logging.py
@@ -10,12 +10,11 @@ job_id = 'legistar_scraper_' + timestamp
 
 # get directories
 scraping_dir = os.path.join(AUTOLOCAL_HOME, 'data', 'scraping')
-legistar_scraper_path = os.path.join(AUTOLOCAL_HOME, 'autolocal', 'scraping', 'legistar_scraper.py')
+legistar_scraper_path = os.path.join(AUTOLOCAL_HOME, 'autolocal', 'scrapers', 'legistar_scraper.py')
 log_path = os.path.join(scraping_dir, 'log.out')
 
 # build and run command with logging
 args = sys.argv[1:]
-print(args)
 scraper_command = [legistar_scraper_path, '--job_id', timestamp]  + args + ['>', log_path]
 run(scraper_command)
 
