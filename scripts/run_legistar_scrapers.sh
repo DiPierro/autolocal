6b6b6ad0124ea1e90091f31cc5fb9bbec8db4bff
Chris Stock <christopher.h.stock@gmail.com>
2019-12-02 18:51:30 -0500

    updates to shell script

--- scripts/run_legistar_scrapers.sh
+++ scripts/run_legistar_scrapers.sh
@@ -1,5 +1,4 @@
-# export PYTHONPATH=/home/ubuntu/autolocal:$PYTHONPATH
-# alias python=/home/ubuntu/miniconda3/envs/autolocal/bin/python 
+#!/bin/bash
 source /home/ubuntu/.bashrc
 conda activate autolocal
 python /home/ubuntu/autolocal/autolocal/scrapers/run_legistar_scraper.py --year 2019 
