ab88b52740d25ec7e69618eab6f43b2dbf17003b
Chris Stock <christopher.h.stock@gmail.com>
2019-12-02 18:05:26 -0500

    added scraping script

---
+++ autolocal/scrapers/scrape_2019.sh
@@ -0,0 +1,2 @@
+export PYTHONPATH=$HOME/autolocal:$PYTHONPATH
+/home/ubuntu/miniconda3/envs/autolocal/bin/python ~/autolocal/autolocal/scrapers/run_legistar_scraper.py --year 2019 1> ~/out/cron.out 2> ~/out/cron.out
