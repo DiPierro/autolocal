f6492cf763c566319fbd698856ae76154a3a6a0b
Chris Stock <christopher.h.stock@gmail.com>
2019-12-02 18:42:53 -0500

    updated run_legistar_scrapers.sh

--- autolocal/scrapers/cron_scrape.sh
+++
@@ -1,6 +0,0 @@
-# export PYTHONPATH=/home/ubuntu/autolocal:$PYTHONPATH
-# alias python=/home/ubuntu/miniconda3/envs/autolocal/bin/python 
-source /home/ubuntu/.bashrc
-conda activate autolocal
-python /home/ubuntu/autolocal/autolocal/scrapers/run_legistar_scraper.py --year 2019 
-python /home/ubuntu/autolocal/autolocal/scrapers/run_legistar_scraper.py --year 2020
--- scripts/run_legistar_scrapers.sh
+++ scripts/run_legistar_scrapers.sh
@@ -1,4 +1,6 @@
-export PYTHONPATH=/home/ubuntu/autolocal:$PYTHONPATH
-alias python=/home/ubuntu/miniconda3/envs/autolocal/bin/python 
+# export PYTHONPATH=/home/ubuntu/autolocal:$PYTHONPATH
+# alias python=/home/ubuntu/miniconda3/envs/autolocal/bin/python 
+source /home/ubuntu/.bashrc
+conda activate autolocal
 python /home/ubuntu/autolocal/autolocal/scrapers/run_legistar_scraper.py --year 2019 
 python /home/ubuntu/autolocal/autolocal/scrapers/run_legistar_scraper.py --year 2020
