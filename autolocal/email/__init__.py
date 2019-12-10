668326a86fe6d65222c5dc04a721246a51292be3
Chris Stock <christopher.h.stock@gmail.com>
2019-12-10 01:21:16 -0800

    beefed up emailing using SES

---
+++ autolocal/email/__init__.py
@@ -0,0 +1,2 @@
+from .senders import SESSender
+from .formats import Email
---
+++ autolocal/email/send_test_email.py
@@ -0,0 +1,33 @@
+SENDER_NAME = "CityCouncilor Agenda Bot"
+SENDER_ADDRESS = 'agendabot@citycouncilor.com'
+RECIPIENT = "chstock@stanford.edu"
+
+SUBJECT = 'Welcome to CityCouncilor!'
+BODY_TEXT = ("Welcome to CityCouncilor!\r\n"
+             "This email was sent from test_ses_emailer.py with Amazon SES using the "
+             "AWS SDK for Python (Boto)."
+            )
+
+BODY_HTML = """<html>
+<head></head>
+<body>
+  <h1>Welcome to CityCouncilor!</h1>
+  <p>This email was sent with
+    <a href='https://aws.amazon.com/ses/'>Amazon SES</a> using the
+    <a href='https://aws.amazon.com/sdk-for-python/'>
+      AWS SDK for Python (Boto)</a>.</p>
+</body>
+</html>
+            """    
+
+from autolocal.email import Email
+
+m = Email(
+        recipient_address=RECIPIENT,
+        subject=SUBJECT,        
+        body_html=BODY_HTML,
+        body_text=BODY_TEXT,
+        sender_name=SENDER_NAME,
+        sender_address=SENDER_ADDRESS)
+
+m.send(sender_args={'logging_address': None})
