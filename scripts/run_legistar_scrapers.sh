783f70d5839e5b3a6c531edd69b46a92be7fb04d
Chris Stock <christopher.h.stock@gmail.com>
2019-12-12 14:24:04 -0800

    updated scraping script

--- scripts/run_legistar_scrapers.sh
+++ scripts/run_legistar_scrapers.sh
@@ -3,5 +3,5 @@ source /home/ubuntu/autolocal/scripts/conda_init.sh
 echo $PATH
 conda activate autolocal
 echo $PYTHONPATH
-python /home/ubuntu/autolocal/autolocal/scrapers/run_legistar_scraper.py --year 2019 
-python /home/ubuntu/autolocal/autolocal/scrapers/run_legistar_scraper.py --year 2020
+python /home/ubuntu/autolocal/autolocal/scraper/run_legistar_scraper.py --year 2019 
+python /home/ubuntu/autolocal/autolocal/scraper/run_legistar_scraper.py --year 2020
