62f8e436050ce1139fc69b53f58f5e4fbd7d7b4f
Chris Stock <christopher.h.stock@gmail.com>
2020-01-16 20:40:05 -0800

    updated legistar scraper for 2020

--- scripts/run_legistar_scrapers.sh
+++ scripts/run_legistar_scrapers.sh
@@ -2,6 +2,5 @@
 source /home/ubuntu/autolocal/scripts/conda_init.sh
 echo $PATH
 conda activate autolocal
-echo $PYTHONPATH
-python /home/ubuntu/autolocal/autolocal/scraper/run_legistar_scraper.py --year 2019 
+echo $PYTHONPATH 
 python /home/ubuntu/autolocal/autolocal/scraper/run_legistar_scraper.py --year 2020
